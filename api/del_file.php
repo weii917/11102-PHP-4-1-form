e5ffc5a6d4b36fc93173a165c3d2369825d2a5f8
wei <wendyhsu09@gmail.com>
2023-11-23 21:28:09 +0800

    新增檔案

---
+++ api/del_file.php
@@ -0,0 +1,12 @@
+<?php
+include_once "../db.php";
+
+$id=$_GET['id'];
+$file=find('files',$id)['name'];
+
+del('files',$id);
+
+unlink('../imgs/'.$file);
+
+
+header("location:../manage.php");
---
+++ imgs/2023112311144890969.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2023-05-05 04:53:03
+-- 伺服器版本： 10.4.27-MariaDB
+-- PHP 版本： 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `home`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `expense`
+--
+
+CREATE TABLE `expense` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `date` date NOT NULL,
+  `note` varchar(64) NOT NULL,
+  `payment` int(11) UNSIGNED NOT NULL,
+  `type` varchar(32) NOT NULL,
+  `sum` int(11) UNSIGNED NOT NULL,
+  `payto` varchar(16) NOT NULL,
+  `item` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `expense`
+--
+ALTER TABLE `expense`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `expense`
+--
+ALTER TABLE `expense`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
