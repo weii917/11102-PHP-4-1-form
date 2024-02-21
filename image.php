548b438eed47938e0ee66719774870ea13f1bfbf
wei <lin951016@gmail.com>
2024-02-21 10:14:55 +0800

    完成圖形檔案處理

--- image.php
+++ image.php
@@ -10,6 +10,55 @@
  * 5.輸出檔案
  */
 
+ if(!empty($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'],'./imgs/'.$_FILES['img']['name']);
+    $source_path='./imgs/'.$_FILES['img']['name'];
+    $type=$_FILES['img']['type'];
+    switch($type){
+        case 'image/jpeg':
+            $source=imagecreatefromjpeg($source_path);
+            list($width,$height)=getimagesize($source_path);
+        break;
+        case 'image/png':
+            $source=imagecreatefrompng($source_path);
+            list($width,$height)=getimagesize($source_path);
+        break;
+        case 'image/gif':
+            $source=imagecreatefromgif($source_path);
+            list($width,$height)=getimagesize($source_path);
+        break;
+        case 'image/bmp':
+            $source=imagecreatefrombmp($source_path);
+            list($width,$height)=getimagesize($source_path);
+        break;
+    }
+    $dst_path='./imgs/small_'.$_FILES['img']['name'];
+    $dst_width=150;
+    $dst_height=200;
+    $dst_source=imagecreatetruecolor($dst_width,$dst_height);
+    imagecopyresampled($dst_source,$source,0,0,0,0,$dst_width,$dst_height,$width,$height);
+    switch($type){
+        case 'image/jpeg':
+            imagejpeg($dst_source,$dst_path);
+        break;
+        case 'image/png':
+            imagepng($dst_source,$dst_path);
+        break;
+        case 'image/gif':
+            imagegif($dst_source,$dst_path);
+        break;
+        case 'image/bmp':
+            imagebmp($dst_source,$dst_path);
+        break;
+    }
+
+    imagedestroy($source);
+    imagedestroy($dst_source);
+
+ }
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,13 +66,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>文字檔案匯入</title>
+    <title>圖形檔案處理</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <h1 class="header">圖形處理練習</h1>
 <!---建立檔案上傳機制--->
-<form action="?" method="post">
+<form action="?" method="post" enctype="multipart/form-data">
     <label for="">選擇檔案:</label>
     <input type="file" name="img" id="">
     <input type="submit" value="上傳">
@@ -31,7 +80,7 @@
 
 
 <!----縮放圖形----->
-
+<img src="<?=$dst_path;?>" alt="">
 
 <!----圖形加邊框----->
 
