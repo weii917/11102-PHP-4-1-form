a26501b7d4fac536bec1128c71c3604a8370d62f
wei <lin951016@gmail.com>
2024-03-01 11:45:53 +0800

    增加可以勾選選擇全部的功能

--- text-export.php
+++ text-export.php
@@ -1,50 +1,54 @@
 <?php
 include "db_export.php";
 
-if(!empty($_POST)){
-$rows=all("20200706"," where  `投票所編號` in ('".join("','",$_POST['select'])."')");
+if (!empty($_POST)) {
+    $rows = all("20200706", " where  `投票所編號` in ('" . join("','", $_POST['select']) . "')");
 
-$filename=date("Ymd").rand(100000000,999999999);
-$file=fopen("./doc/{$filename}.csv",'w+');
-fwrite($file, "\xEF\xBB\xBF");
-$chk=false;
-foreach($rows as $row){
-    if(!$chk){
-        $cols=array_keys($row);
-        fwrite($file,join(",",$cols)."\r\n");
-        $chk=true;
+    $filename = date("Ymd") . rand(100000000, 999999999);
+    $file = fopen("./doc/{$filename}.csv", 'w+');
+    fwrite($file, "\xEF\xBB\xBF");
+    $chk = false;
+    foreach ($rows as $row) {
+        if (!$chk) {
+            $cols = array_keys($row);
+            fwrite($file, join(",", $cols) . "\r\n");
+            $chk = true;
+        }
+        fwrite($file, join(",", $row) . "\r\n");
     }
-    fwrite($file,join(",",$row)."\r\n");
-}
-fclose($file);
+    fclose($file);
 
-echo "<a href='./doc/{$filename}.csv'  download>檔案已匯出，請點此連結下載</a>";
+    echo "<a href='./doc/{$filename}.csv'  download>檔案已匯出，請點此連結下載</a>";
 }
 
 
 ?>
 <style>
-table {
-    border-collapse: collapse;
-}
+    table {
+        border-collapse: collapse;
+    }
 
-td {
-    border: 1px solid #666;
-    padding: 5px 12px;
-}
+    td {
+        border: 1px solid #666;
+        padding: 5px 12px;
+    }
 
-th {
-    border: 1px solid #666;
-    padding: 5px 12px;
-    background-color: black;
-    color: white;
-}
+    th {
+        border: 1px solid #666;
+        padding: 5px 12px;
+        background-color: black;
+        color: white;
+    }
 </style>
+<script src="./jquery-3.4.1.min.js"></script>
 <form action="?" method="post">
     <input type="submit" value="匯出選擇的資料">
     <table>
         <tr>
-            <th>勾選</th>
+            <th>
+                <input type="checkbox" name="" id="select">
+                勾選
+            </th>
             <th>投票所編號</th>
             <th>投票所</th>
             <th>候選人1</th>
@@ -63,21 +67,30 @@ th {
             <th>投票率</th>
         </tr>
         <?php
-$rows=all('20200706');
-foreach($rows as $row){
-    echo "<tr>";
-    echo "<td>";
-    echo "<input type='checkbox' name='select[]' value='{$row['投票所編號']}'>";
-    echo "</td>";
-    foreach($row as $value){
-        echo "<td>";
-        echo $value;
-        echo "</td>";
-    }
-    echo "</tr>";
-}
+        $rows = all('20200706');
+        foreach ($rows as $row) {
+            echo "<tr>";
+            echo "<td>";
+            echo "<input type='checkbox' name='select[]' value='{$row['投票所編號']}'>";
+            echo "</td>";
+            foreach ($row as $value) {
+                echo "<td>";
+                echo $value;
+                echo "</td>";
+            }
+            echo "</tr>";
+        }
 
 
-?>
+        ?>
     </table>
 </form>
+<script>
+    $("#select").on("change", function() {
+        if ($(this).prop('checked')) {
+            $("input[name='select[]']").prop('checked', true);
+        } else {
+            $("input[name='select[]']").prop('checked', false);
+        }
+    })
+</script>
