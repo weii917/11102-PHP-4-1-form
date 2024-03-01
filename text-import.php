31e09eb58a184f798200f5118a628d641c047e7c
wei <lin951016@gmail.com>
2024-03-01 09:54:08 +0800

    讀取csv檔顯示在畫面上

--- text-import.php
+++ text-import.php
@@ -1,4 +1,15 @@
+<style>
+    table {
+        border-collapse: collapse;
+    }
+
+    td {
+        border: 1px solid #666;
+        padding: 5px 12px;
+    }
+</style>
 <?php
+
 /****
  * 1.建立資料庫及資料表
  * 2.建立上傳檔案機制
@@ -9,9 +20,42 @@
  * 7.結束檔案
  */
 
+if (!empty($_FILES['text']['tmp_name'])) {
+    $filename = $_FILES['text']['name'];
+    $filesize = $_FILES['text']['size'];
+    echo "檔案上傳成功!檔名為:" . $filename;
+    echo "<br>檔案大小:" . $filesize;
+    echo "<br>";
+    $file = fopen($_FILES['text']['tmp_name'], 'r');
+    $line = fgets($file);
+
+    $cols = explode(",", $line);
+    echo "<table>";
+    echo "<tr>";
+    foreach ($cols as $col) {
+        echo "<th>";
+        echo $col;
+        echo "</th>";
+    }
+    echo "</tr>";
+    while (!feof($file)) {
+        $line = fgets($file);
+        $cols = explode(",", $line);
+        echo "<tr>";
+        foreach ($cols as $col) {
+            echo "<td>";
+            echo $col;
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,15 +63,21 @@
     <title>文字檔案匯入</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<h1 class="header">文字檔案匯入練習</h1>
-<!---建立檔案上傳機制--->
+    <h1 class="header">文字檔案匯入練習</h1>
+    <!---建立檔案上傳機制--->
+    <form action="?" method="post" enctype="multipart/form-data">
+        <input type="file" name="text" id="">
+        <input type="submit" value="送出">
+
+    </form>
 
 
-
-<!----讀出匯入完成的資料----->
+    <!----讀出匯入完成的資料----->
 
 
 
 </body>
+
 </html>
